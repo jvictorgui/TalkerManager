4839f4d4c057d78dd675248d85ab0fc0136375b4
joao <druvao136@gmail.com>
2023-08-12 00:52:59 -0300

    req 4, lint e comentarios

--- src/middlewares/validateLoginFields.js
+++ src/middlewares/validateLoginFields.js
@@ -1,23 +1,41 @@
-// const readFile = require('../helpers/fsFuncs');
+// valida os campos de Email
+const validateEmailFields = async (req, res, next) => {
+    // Obtém o email do corpo da requisição
+    const { email } = req.body;
 
-const validateFields = async (req, res, next) => {
-const { email, password } = req.body;
-const emailRegex = /\S+@\S+\.\S+/;
+    // Regex para validar o formato de email
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    // Verifica se o campo "email" está ausente
+    if (!email) {
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    }
+
+    // Verifica se o formato do email é válido usando p Regex
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+
+    next();
+};
+
+const validatePasswordsFields = async (req, res, next) => {
+    const { password } = req.body;
+// Regex para validar o comprimento mínimo da senha
 const passwordRegex = /\d{6,}/;
-if (!email) {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-}
-if (emailRegex.test(email) === false) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-} 
+
+// Verifica se o campo "password" está ausente
 if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
 }
 
-if (passwordRegex.test(password) === false) {
+// Verifica se o comprimento da senha é válido usando o Regex
+if (!passwordRegex.test(password)) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
 }
 next();
 };
-
-module.exports = validateFields;
+module.exports = {
+    validateEmailFields,
+    validatePasswordsFields,
+};
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,14 +1,28 @@
 const loginRouter = require('express').Router();
-const generateRandomToken = require('../helpers/crypto');
-const validateFields = require('../middlewares/validateLoginFields');
 
-loginRouter.post('/', validateFields, (req, res) => {
+// função de geração de token aleatório 
+const generateRandomToken = require('../helpers/crypto');
+
+// middleware que valida os campos de Email e Password
+const {
+    validateEmailFields,
+    validatePasswordsFields,
+} = require('../middlewares/validateLoginFields');
+
+loginRouter.post('/', validateEmailFields, validatePasswordsFields, (req, res) => {
+    // Obtém o email e a senha do corpo da requisição
     const { email, password } = req.body;
+
+    // Verifica se o email ou a senha estão ausentes
     if (!email || !password) {
         return res.status(401).send('email ou senha invalidos');
     }
+
+    // Gera um token aleatório
     const token = generateRandomToken(16);
+
+    // Retorna uma resposta de sucesso com o token e uma mensagem
     res.status(200).json({ token, message: 'Login efetuado com sucesso' });
 });
 
-module.exports = loginRouter; 
+module.exports = loginRouter;
